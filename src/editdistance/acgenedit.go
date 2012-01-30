228fd0e189fcb76c11dd35851d31f7a81d94195f
Jani Rahkola <jani.rahkola@gmail.com>
2012-01-30 18:36:53 +0200

    diagonal doubling for acgenedit

--- src/editdistance/acgenedit.go
+++ src/editdistance/acgenedit.go
@@ -30,32 +30,50 @@ func MakeACGenEdit(G [][]string, c []float64) func(string, string) float64 {
 
 	return func(A string, B string) float64 {
 		d := makeMatrix(len(B)+1, len(A)+1)
-
-		Astate := Aroot
-		Bstate := Broot
-
-		for x := 0; x <= len(A); x++ {
-			Bstate = Broot
-			for y := 0; y <= len(B); y++ {
-				if x == 0 && y == 0 {
-					d[y][x] = 0
-				} else {
-					d[y][x] = minCost(A[:x],
-						Astate,
-						B[:y],
-						Bstate,
-						d)
-				}
-
-				if y < len(B) {
-					Bstate = Bstate.Push(B[y])
-				}
-			}
-			if x < len(A) {
-				Astate = Astate.Push(A[x])
+		for x := 0; x < len(A)+1; x++ {
+			for y := 0; y < len(B)+1; y++ {
+				d[y][x] = math.Inf(1)
 			}
 		}
 
-		return d[len(B)][len(A)]
+		prevBest := math.Inf(1)
+		yStart := -1
+		colHeight := 2
+		for colHeight / 2 < len(B)+1 {
+			Astate := Aroot
+			for x := 0; x < len(A)+1; x++ {
+				Bstate := Broot
+				for y := IntMax(yStart, 0);
+				y < IntMin(yStart+colHeight, len(B)+1);
+				y++ {
+					if x == 0 && y == 0 {
+						d[y][x] = 0
+					} else {
+						d[y][x] = minCost(A[:x],
+							Astate,
+							B[:y],
+							Bstate,
+							d)
+					}
+
+					if y < len(B) {
+						Bstate = Bstate.Push(B[y])
+					}
+				}
+				if x < len(A) {
+					Astate = Astate.Push(A[x])
+				}
+			}
+
+			newBest := math.Fmin(d[len(B)][len(A)], prevBest)
+			if newBest == prevBest && !math.Inf(newBest, 1) {
+				return newBest
+			}
+			prevBest = newBest
+
+			yStart = yStart - yStart
+			colHeight = colHeight * 2
+		}
+		return prevBest
 	}
 }
